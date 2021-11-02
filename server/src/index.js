84de438223ba4b2a0719a8863c350defffc8b33a
Soobin <ssbin93@gmail.com>
2021-11-02 23:27:26 +1000

    Install GraphQL

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,29 @@
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
 import cors from "cors";
 import messagesRoute from "./routes/messages.js";
 import usersRoute from "./routes/users.js";
 
 const app = express();
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
 
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true
 }));
 
-const routes = [...messagesRoute, ...usersRoute];
-routes.forEach(({ method, route, handler}) => {
-  app[method](route, handler);
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  context: {
+    models: {
+      messages: '',
+      users: ''
+    }
+  }
 });
+
+server.applyMiddleware({ app, path: '/graphql' });
 
 app.listen(8000, () => {
   console.log('server listening on 8000...');
-})
+});
